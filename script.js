ff9e2d188b17cd3fb4ed16302e42a742ecafaa0e
copybyayaulym <ayaulym.tungyshbay@invisionu.education>
2026-02-25 01:40:44 +0500

    Added_jscript
    Removed starter comments for student implementation steps.

--- script.js
+++ script.js
@@ -39,9 +39,4 @@ function calculateSalary() {
          Tax amount: ${formattedTax} ₸ <br>
          Net salary: ${formattedNet} ₸ <br>
          Income category: ${category}`;
-}// Starter JS
-// Steps for students to implement:
-// 1. Get user inputs
-// 2. Perform calculations
-// 3. Use if/else to classify results
-// 4. Display results in #output
+
